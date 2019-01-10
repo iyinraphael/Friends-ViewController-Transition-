b02122188c387d58fcaf2200bbed236dd79432bf
iyinraphael <iyinraphael@gmail.com>
2019-01-10 13:04:22 -0800

    set tableViw and Detailview in storyboard and connected outlets to its subview

---
+++ Friends (ViewController Transition)/ViewController/FriendTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FriendTableViewCell.swift
+//  Friends (ViewController Transition)
+//
+//  Created by Iyin Raphael on 1/10/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var friendImageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
